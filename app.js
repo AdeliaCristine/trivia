d21b490be00ae62a14b415cd8f2873c31e81b3d5
AdeliaCristine <60454824+AdeliaCristine@users.noreply.github.com>
2020-03-26 22:20:20 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,23 +1,77 @@
 function nome(){
     const nome =document.getElementById("nome").value;
-    document.getElementById("ola").innerHTML = "Olá " + nome + ". Deseja jogar?"
- 
+    document.getElementById("nome").style.display="none";
+    document.getElementById("botao").style.display="none";
+    document.getElementById("ola").innerHTML = "Olá " + nome + ". Você deseja jogar?";
+    document.getElementById("jogo").style.display="block";
+    document.getElementById("in").style.display="none";
 }
 function sim(){
     document.getElementById("sim").style.display="none";
-    document.getElementById("resposta").innerHTML= "Escolha um tema."
+    document.getElementById("nao").style.display="none";
+    document.getElementById("resposta").innerHTML= "Escolha um tema.";
+    document.getElementById("tema1").style.display="none";
+    document.getElementById("tema").style.display="block";
+    document.getElementById("ola").style.display="none";
+    document.getElementById("tema2").style.display="none"
 }
 function nao(){
     document.getElementById("nao").style.display="none";
+    document.getElementById("sim").style.display="none";
     document.getElementById("resposta").innerHTML= "Até logo!"
+    window.close();
 }
 function desenho(){
-    var perg1 = prompt ("Qual o nome da namorada do Mickey? \n Escolha o numero da sua resposta. \n 1. Pateta \n 2.Minnie \n 3.Margarida");
-    var perg2 = prompt ("Quem era o dono do cofre de moedas de ouro? \n Escolha o numero da sua resposta. \n 1. Tio Patinha \n 2.Pluto \n 3.Huguinho");
-    var perg3 = prompt ("Quem tem medo de água? \n Escolha o numero da sua resposta. \n 1. Monica \n 2.Anjinho \n 3.Cascão");
-}
+    document.getElementById("comida").style.display="none";
+    document.getElementById("desenho").style.display="none";
+    document.getElementById("tema1").style.display="block";
+    document.getElementById("resposta").style.display="none";
+}  
 function comida(){
-    var perg1 = prompt ("Qual dessas é uma PANC ( Plantas Alimenticias Não Convencionais? \n Escolha o numero da sua resposta. \n 1. Ora-pro-nobis \n 2.Aipim \n 3.Feijão");
-    var perg2 = prompt ("Qual dessas não é uma raiz? \n Escolha o numero da sua resposta. \n 1. Batata \n 2.Aipim \n 3.uva");
-    var perg3 = prompt ("Qual desses é não é derivado de leite? \n Escolha o numero da sua resposta. \n 1. Queijo \n 2.Presunto \n 3.Requeijão");
+    document.getElementById("comida").style.display="none";
+    document.getElementById("desenho").style.display="none";
+    document.getElementById("tema2").style.display="block";
+    document.getElementById("resposta").style.display="none";
 }
+function env1(){
+    document.getElementById("tema1").style.display="none";
+    document.getElementById("recomeçar").style.display="block";
+    document.getElementById("final").style.display="block";
+    //document.getElementById("1").innerHTML= Resposta 1;
+    //document.getElementById("2").innerHTML= Resposta 1;
+    //document.getElementById("3").innerHTML= Resposta 1;
+}  
+function env2(){
+  document.getElementById("tema2").style.display="none";
+  document.getElementById("recomeçar").style.display="block";
+  document.getElementById("final").style.display="block";
+
+}
+function jogar(){
+    document.getElementById("joga").innerHTML= "Escolha um tema.";
+    document.getElementById("jn").style.display="none";
+    document.getElementById("sair").style.display="none";
+    document.getElementById("final").style.display="none";
+    document.getElementById("tema").style.display="block";
+    repeat function sim(){
+
+    
+}
+function sair(){
+  window.close();
+}
+//(if https://www.tutorialspoint.com/css/css_text-decoration.htm(resp1== c1){
+  //  document.getElementById('1').innerHTML= "Resposta 1"
+//} else {
+  //  document.getElementById('4').innerHTML= "Resposta 1";
+//} 
+//if (perg2 == 1){
+  //  document.getElementById('r2').innerHTML= "Resposta 2"
+//} else {
+  //  document.getElementById('e2').innerHTML= "Resposta 2";
+//} 
+//if (perg3 == 3){
+  //  document.getElementById('r3').innerHTML= "Resposta 3"
+//} else {
+  //  document.getElementById('e3').innerHTML= "Resposta 3";
+//} )
